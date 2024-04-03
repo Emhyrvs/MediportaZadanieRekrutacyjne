007e53011d4d7c209b7e9db8ce04537221b2340f
maciej.zakrzewski <maciej.zakrzewski@bcoders.pl>
2024-04-03 15:31:48 +0200

    All test plus dockerize

--- MediportaZadanieRekrutacyjne/Program.cs
+++ MediportaZadanieRekrutacyjne/Program.cs
@@ -22,7 +22,10 @@ builder.Services.AddCors(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My API", Version = "v1" });
+});
 builder.Services.AddScoped<IStackExchangeService, StackExchangeService>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddDbContext<DataDbContext>(options =>
@@ -36,8 +39,12 @@ Log.Logger = new LoggerConfiguration()
 builder.Services.AddSingleton(Log.Logger);
 var app = builder.Build();
 
-
-
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+    c.RoutePrefix = string.Empty; // Dla dostępu do Swagger UI pod głównym adresem
+});
 app.UseHttpsRedirection();
 
 
